997427f864297778328603242819f33d6b3ef66d
郑友捷 <Azure_stars@126.com>
2024-04-30 21:34:05 +0800

    test: remove unit test, which is replaced by CI in the future

--- make/test.mk
+++ make/test.mk
@@ -1,11 +1,5 @@
 # Test scripts
 
-define unit_test
-  $(call run_cmd,cargo test,-p percpu $(1) -- --nocapture)
-  $(call run_cmd,cargo test,-p axfs $(1) --features "myfs" -- --nocapture)
-  $(call run_cmd,cargo test,--workspace --exclude "arceos-*" --exclude "monolithic*" --exclude "axstarry*" --exclude "axprocess" $(1) -- --nocapture)
-endef
-
 test_app :=
 ifneq ($(filter command line,$(origin A) $(origin APP)),)
   test_app := $(APP)
