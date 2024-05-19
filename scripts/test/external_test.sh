540754dfb0f0630c461f2dc1bc09d1cc69d20948
Youjie Zheng <Azure_stars@126.com>
2024-05-20 00:18:08 +0800

    chore: use `dependencies-patch` crates to do patch

--- scripts/test/external_test.sh
+++ scripts/test/external_test.sh
@@ -1,8 +1,6 @@
 # The directory of the main repo
 WORK_DIR=$1
 
-PATCH_TOOL_DIR=$WORK_DIR/tools/patch_tool
-
 # The package which needs to be patched
 # It always be the package which triggers the test
 PATCH_PACKAGE=$2
@@ -18,5 +16,6 @@ mkdir .github/workflows/actions
 mv $WORK_DIR/.github/workflows/actions/* .github/workflows/actions
 
 # To do the patch for current commit
-cd $PATCH_TOOL_DIR
-cargo run -- ../../ $PATCH_PACKAGE $PATCH_TARGET_URL $PATCH_COMMIT_HASH
+cargo install dependencies-patch
+# cargo run -- ../../ $PATCH_PACKAGE $PATCH_TARGET_URL $PATCH_COMMIT_HASH
+dependencies-patch -c $WORK_DIR -n $PATCH_PACKAGE --git-repo $PATCH_TARGET_URL --commit $PATCH_COMMIT_HASH
