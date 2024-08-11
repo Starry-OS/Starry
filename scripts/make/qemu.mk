e68b617cd54d1adcf269d0644dcd672f31d98725
Youjie Zheng <Azure_stars@126.com>
2024-08-11 17:52:46 +0800

    feat: qemu use 8G physical memory

--- scripts/make/qemu.mk
+++ scripts/make/qemu.mk
@@ -25,7 +25,7 @@ qemu_args-aarch64 := \
   -kernel $(OUT_BIN)
 
 # bitmap_allocator is hard coding, support max 4GB mem
-qemu_args-y := -m 2G -smp $(SMP) $(qemu_args-$(ARCH))
+qemu_args-y := -m 8G -smp $(SMP) $(qemu_args-$(ARCH))
 
 qemu_args-$(BLK) += \
   -device virtio-blk-$(vdev-suffix),drive=disk0 \
