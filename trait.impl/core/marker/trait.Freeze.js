82d737a8aae43fc7643a3ba9ff7faf736eb9e5a7
Azure-stars <Azure_stars@126.com>
2024-10-28 03:56:32 +0000

    Deploying to gh-pages from @ Starry-OS/Starry@5b8daa2f38658758061c53bc6caecff30e3c875f 🚀

---
+++ trait.impl/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"percpu":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"percpu/struct.EXAMPLE_PERCPU_DATA_WRAPPER.html\" title=\"struct percpu::EXAMPLE_PERCPU_DATA_WRAPPER\">EXAMPLE_PERCPU_DATA_WRAPPER</a>",1,["percpu::EXAMPLE_PERCPU_DATA_WRAPPER"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
